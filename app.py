ddd17eba6cd0e6c47b7cad6dc36f1d5ffbdd484a
Thisisswich <thisisswich@gmail.com>
2025-05-30 16:20:14 -0700

    Update app.py
    added Flask debug output

--- app.py
+++ app.py
@@ -51,3 +51,5 @@ def trigger():
     logging.info(f"/trigger endpoint received payload: {data}")
     return jsonify({"status": "success", "received": data}), 200
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
