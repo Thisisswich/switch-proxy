7a18f6f36927d580fb0127f5e5e90daaa3e63f61
Thisisswich <thisisswich@gmail.com>
2025-05-29 18:58:21 -0700

    Updated Stripe keys and plug IPs

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ STRIPE_WEBHOOK_SECRET = "whsec_cUSpoPShjKaFJmAtCr0oxtNrO7aoGKro"
 
 # Mapping Stripe price_id → Shelly config
 DEVICE_MAP = {
-    "price_123": {"device_ip": "192.168.1.101", "duration": 30, "pi_ip": "192.168.1.50"},
+    "price_1RIDIh09QLR8NlKt2YfQ69vc": {"device_ip": "192.168.0.244", "duration": 1, "pi_ip": "192.168.0.154"},
     "price_456": {"device_ip": "192.168.1.102", "duration": 60, "pi_ip": "192.168.1.50"},
 }
