183901a12a2fe581b379e57f4bae6481f8c13656
Thisisswich <thisisswich@gmail.com>
2025-06-10 12:01:42 -0700

    Update config.py
    updated for security issue to start using .env file

--- config.py
+++ config.py
@@ -1,18 +1,10 @@
+import os
+from dotenv import load_dotenv
+import json
 import stripe
 
-stripe.api_key = "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"
+load_dotenv()
 
-STRIPE_WEBHOOK_SECRET = "whsec_cUSpoPShjKaFJmAtCr0oxtNrO7aoGKro"
-
-DEVICE_MAP = {
-    "price_1RIDIh09QLR8NlKt2YfQ69vc": {
-        "device_ip": "192.168.0.244",
-        "duration": 60,
-        "tunnel_url": "https://pi.swichswich.com"
-    },
-    "price_456": {
-        "device_ip": "192.168.1.102",
-        "duration": 60,
-        "tunnel_url": "https://pi.swichswich.com"
-    },
-}
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+STRIPE_WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
+DEVICE_MAP = json.loads(os.getenv("DEVICE_MAP"))
